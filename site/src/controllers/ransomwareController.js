e88457853731453903a67d0828f431f2d5d9ab93
ak-miyuki <andreia.kubota@sptech.school>
2026-06-07 03:19:35 -0300

    select e titulo dos graficos

--- site/src/controllers/ransomwareController.js
+++ site/src/controllers/ransomwareController.js
@@ -3,6 +3,8 @@ const ransomwareModel = require("../models/ransomwareModel");
 
 function buscarDadosRansomware(req, res) {
 
+    console.log("QtdLinhas recebida:", req.body.QtdLinhas);
+    
     const mac = req.body.MacServer;
 
     ransomwareModel.buscarDadosRansomware(mac)
